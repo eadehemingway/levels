d3fc32c458eb260a22c585a34c90831ca0fe0165
eadehemingway <eade.hemingway@gmail.com>
2020-04-28 21:20:07 +0100

    add frombasics page

---
+++ src/pages/frombasics.tsx
@@ -0,0 +1,179 @@
+import React from "react"
+import * as d3 from "d3"
+import styled from "styled-components"
+
+class Force extends React.Component {
+  svgWidth = 700
+  svgHeight = 500
+  state = {
+    data: [
+      {
+        id: 2,
+        value: 2,
+        campus: "khaleel",
+        gender: "female",
+        role: "student",
+      },
+      { id: 3, value: 3, campus: "khaleel", gender: "male", role: "student" },
+      { id: 4, value: 4, campus: "khaleel", gender: "male", role: "student" },
+      { id: 6, value: 6, campus: "khaleel", gender: "female", role: "mentor" },
+      { id: 7, value: 7, campus: "khaleel", gender: "female", role: "mentor" },
+      {
+        id: 8,
+        value: 8,
+        campus: "khaleel",
+        gender: "female",
+        role: "student",
+      },
+      { id: 10, value: 10, campus: "gaza", gender: "female", role: "student" },
+      { id: 11, value: 11, campus: "gaza", gender: "male", role: "student" },
+      { id: 12, value: 12, campus: "gaza", gender: "male", role: "student" },
+      { id: 14, value: 14, campus: "gaza", gender: "female", role: "mentor" },
+      { id: 15, value: 15, campus: "gaza", gender: "female", role: "mentor" },
+      { id: 16, value: 16, campus: "gaza", gender: "female", role: "student" },
+    ],
+    categories: {
+      role: ["student", "mentor"],
+      campus: ["gaza", "khaleel"],
+      gender: ["male", "female"],
+    },
+    value: "role",
+  }
+
+  componentDidMount() {
+    const { data } = this.state
+    const svg = d3
+      .select("svg")
+      .attr("width", this.svgWidth)
+      .attr("height", this.svgHeight)
+
+    svg
+      .selectAll("circle")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("r", d => d.value * 2)
+      .attr("class", "bubble")
+      .attr("fill", d => (d.gender === "male" ? "coral" : "#c4c3d0"))
+
+    this.updateLabels()
+    this.createSimulation()
+  }
+
+  // works out the coordinates of the groups, and adds 'group' property which can be read later to use for the labels of each group
+  centerOfGravityForGroups = categories => {
+    const firstCenter = this.svgWidth / 4
+    const secondCenter = firstCenter * 3
+    const yVal = this.svgHeight / 2
+    const [groupOne, groupTwo] = categories
+    return [
+      { x: firstCenter, y: yVal, group: groupOne },
+      { x: secondCenter, y: yVal, group: groupTwo },
+    ]
+  }
+
+  // this takes a data point and works out which category it should be in
+  centerOfGravityForDatum = d => {
+    const { categories, value } = this.state
+    const dataGroupArr = categories[value]
+    const dataGroup = d[value]
+    return this.centerOfGravityForGroups(dataGroupArr).find(
+      e => e.group === dataGroup
+    )
+  }
+
+  createSimulation = () => {
+    const { data } = this.state
+
+    const forceX = d3
+      .forceX()
+      .x(d => this.centerOfGravityForDatum(d).x)
+      .strength(1)
+
+    const forceY = d3
+      .forceY()
+      .y(d => this.centerOfGravityForDatum(d).y)
+      .strength(1)
+
+    d3.forceSimulation(data)
+      .force(
+        "collision",
+        d3.forceCollide().radius(d => d.value * 2)
+      )
+      .force("x", forceX)
+      .force("y", forceY)
+      .alpha(0.1) // small alpha to have the elements move at a slower pace
+      .on("tick", () => {
+        // call the tick function running the simulation
+        d3.selectAll(".bubble").attr(
+          "transform",
+          d => `translate(${d.x} ${d.y})`
+        )
+      })
+  }
+
+  updateLabels = () => {
+    const { categories, value } = this.state
+    const dataGroupArr = categories[value]
+    const updatedCategoriesCenter = this.centerOfGravityForGroups(dataGroupArr)
+
+    const labelSelection = d3
+      .select("svg")
+      .selectAll("text")
+      .data(updatedCategoriesCenter)
+
+    labelSelection.exit().remove()
+
+    const entering = labelSelection
+      .enter()
+      .append("text")
+      .style("font-size", "20")
+      .style("letter-spacing", "0.1rem")
+      .attr("text-anchor", "middle")
+      .attr("dominant-baseline", "middle")
+
+    const updatedAndEnter = labelSelection.merge(entering)
+
+    updatedAndEnter
+      .text(d => d.group)
+      .attr("x", d => d.x)
+      .attr("opacity", "0.4")
+      .attr("font-family", "futura")
+      .attr("fill", "lightslategray")
+      .attr("y", 100)
+  }
+
+  redraw = value => {
+    this.setState({ value }, () => {
+      this.updateLabels()
+      this.createSimulation()
+    })
+  }
+  render() {
+    const { categories } = this.state
+    const selectCategories = Object.keys(categories)
+    return (
+      <section className="page-excl-nav">
+        <div className="force-directed-page">
+          <StyledSelect onChange={e => this.redraw(e.target.value)}>
+            {selectCategories.map(c => (
+              <option key={c} value={c}>
+                {c}
+              </option>
+            ))}
+          </StyledSelect>
+          <svg></svg>
+        </div>
+      </section>
+    )
+  }
+}
+
+const StyledSelect = styled.select`
+  padding: 20px;
+  width: 200px;
+  font-size: 20px;
+  margin: 70px;
+`
+
+export default Force
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -9,127 +9,63 @@ const data = [
   {
     name: "a",
     code: "one",
-    GDP: {
-      "1990": 1452.87747911376,
-      "2010": 34408.16861192198,
+    xVal: {
+      1: 200,
+      2: 800,
     },
   },
-  // {
-  //   name: "b",
-  //   code: "two",
-  //   GDP: {
-  //     "1990": 1452.87747911376,
-  //     "2010": 34408.16861192198,
-  //   },
-  // },
-  // {
-  //   name: "c",
-  //   code: "three",
-  //   GDP: {
-  //     "1990": 1452.87747911376,
-  //     "2010": 34408.16861192198,
-  //   },
-  // },
-  // {
-  //   name: "d",
-  //   code: "four",
-  //   GDP: {
-  //     "1990": 1452.87747911376,
-  //     "2010": 34408.16861192198,
-  //   },
-  // },
 ]
-const IndexPage = () => {
-  const [year, setYear] = useState(1990)
 
+const forceX = d3
+  .forceX()
+  .x(d => {
+    return d.xVal[1]
+  })
+  .strength(1)
+
+const forceY = d3
+  .forceY()
+  .y(d => 300)
+  .strength(1)
+
+const collision = d3.forceCollide(10 * 2).strength(0.2)
+
+const simulation = d3
+  .forceSimulation(data, d => d.code)
+  .force("collision", collision)
+  .force("x", forceX)
+  .force("y", forceY)
+
+simulation.on("tick", () => {
+  d3.selectAll(`.circle`)
+    .attr("cy", d => d.y)
+    .attr("cx", d => {
+      return d.x
+    })
+})
+const IndexPage = () => {
+  const [number, setNumber] = useState(1)
   const radius = 10
 
   function createSim() {
-    const forceX = d3
-      .forceX()
-      .x(d => findCenterOfGravity(d).x)
-      .strength(1)
-
-    const forceY = d3
-      .forceY()
-      .y(d => findCenterOfGravity(d).y)
-      .strength(1)
-
-    const collision = d3.forceCollide(radius * 2).strength(0.2)
-
-    const simulation = d3
-      .forceSimulation(data, d => d.code)
-      .force("collision", collision)
-      .force("x", forceX)
-      .force("y", forceY)
-
-    simulation.on("tick", () => {
-      d3.selectAll(`.circle`)
-        .attr("cy", d => d.y)
-        .attr("cx", d => {
-          return d.x
-        })
-    })
-
     simulation.alpha(1).restart()
   }
   function setTimeoutYear() {
-    if (year >= 2010) return
+    if (number >= 2) return
     let id = setTimeout(() => {
-      setYear(year + 20)
+      setNumber(number + 1)
     }, 1000)
     return () => clearTimeout(id)
   }
 
   useEffect(() => {
-    drawLabels()
     drawCircles()
-    createSim()
     setTimeoutYear()
   }, [])
 
   useEffect(() => {
-    // console.log(simulation)
     createSim()
-    // simulation.alpha(1).restart()
-  }, [year])
-
-  function findCenterOfGravity(data) {
-    const level = findLevel(data.GDP[year])
-
-    const centersOfGravity = {
-      levelOne: {
-        x: 200,
-        y: 300,
-      },
-      levelTwo: {
-        x: 400,
-        y: 300,
-      },
-      levelThree: {
-        x: 600,
-        y: 300,
-      },
-      levelFour: {
-        x: 800,
-        y: 300,
-      },
-    }
-    return centersOfGravity[level]
-  }
-
-  function findLevel(GDP: number) {
-    switch (true) {
-      case GDP < 2300:
-        return "levelOne"
-      case GDP < 8000:
-        return "levelTwo"
-      case GDP < 21000:
-        return "levelThree"
-      case GDP > 21000:
-        return "levelFour"
-    }
-  }
+  }, [number])
 
   function drawCircles() {
     const svg = d3.select("svg")
@@ -137,6 +73,14 @@ const IndexPage = () => {
       .append("line")
       .attr("x1", 800)
       .attr("x2", 800)
+      .attr("y1", 0)
+      .attr("y2", 800)
+      .attr("stroke", "black")
+      .attr("stroke-width", 2)
+    svg
+      .append("line")
+      .attr("x1", 200)
+      .attr("x2", 200)
       .attr("y1", 0)
       .attr("y2", 800)
       .attr("stroke", "black")
@@ -156,26 +100,6 @@ const IndexPage = () => {
         }
         return "white"
       })
-  }
-
-  function drawLabels() {
-    const labels = [
-      { text: "level one", x: 200, y: 100 },
-      { text: "level four", x: 800, y: 100 },
-    ]
-    const labelSelection = d3.select("svg").selectAll("text").data(labels)
-    labelSelection
-      .enter()
-      .append("text")
-      .text(d => d.text)
-      .attr("y", d => d.y)
-      .attr("x", d => d.x)
-      .style("font-size", "20")
-      .style("letter-spacing", "0.1rem")
-      .attr("text-anchor", "middle")
-      .attr("dominant-baseline", "middle")
-      .attr("fill", "lightslategray")
-      .attr("font-family", "Major Mono")
   }
 
   return <StyledSVG />
